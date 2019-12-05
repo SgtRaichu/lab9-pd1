2923cebe685d34b12c82ea0cfab781b806f12ff3
SgtRaichu <58303013+SgtRaichu@users.noreply.github.com>
2019-12-05 15:23:57 +0300

    Add files via upload

---
+++ İsimsiz11.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+//1
+/*
+/
+  int a, b, c, d;
+  double kok1, kok2;
+
+  printf("Bu formatta giriniz a*x*x + b*x + c = 0\n");
+  scanf("%d%d%d", &a, &b, &c);
+
+  d = b*b - 4*a*c;
+
+  if (d < 0) { 
+    printf("reel kok yok\n");
+  
+  }
+  else { 
+    root1 = (-b + sqrt(d))/(2*a);
+    root2 = (-b - sqrt(d))/(2*a);
+
+    printf("1. kok = %.2lf\n", kok1);
+    printf("2. kok  = %.2lf\n", kok2);
+  }
+
+  return 0;
+  */
+  //2
+  /*
+  int i,j,N=0;
+	
+	printf("Enter a N:\n");
+	scanf("%d",&N);
+	
+	for(i=1 ; i<N; i++){
+		
+		for(j=1 ; j<N ; j++){
+			
+			if((i*i)+(j*j)==N)
+				printf("i:%d and j:%d \n",i,j);
+			else
+				j++;
+		}
+	}
+  */
+  
+  
+  
+  
+  
+  
+  //3
+  /*
+  for(int i=100;i<200;i++){
+  	
+  	
+  	if(i%9==0){
+  		
+  		printf("%d\n",i);
+  		
+	  }
+  	
+  	else i++;
+  	
+  }
+  */
+  //4
+  /*
+  int dizi[5]={0,2,4,6,8};
+  int sum=0;
+  int ort ;
+  
+  
+  for(int i;i<5;i++){
+  	
+  	
+  	sum=sum+dizi[i];
+  	
+  }
+  printf("%d\n",sum);
+  ort=sum/5;
+  printf("%d",ort);
+  */
+  
+  
+  
+}
